bdc3efe8f52687177ebefd5fcebed8adc7ad860b
kenLukau31 <kenlukau@gmail.com>
2024-01-19 19:13:52 +0000

    underline

--- java.script/login.js
+++ java.script/login.js
@@ -1,15 +1,18 @@
-function login_form_upValidate() {
-    let email = document.getElementByI("email").value;
+function login_form_Validate() {
+    // aceder ao valor do elemento html com id="email"
+    let email = document.getElementById("email").value; 
+    // msg de alerta
     window.alert("Bem vindo(a)! \nEmail: " + email);
     
 }
 
 function forgotpassword() {
-    let email = document.forms("email").value;
-
-   if (hkk){
-        window.alert("Um link de indereço foi  enviado para o seu email \n "+ email);
+    let email = document.getElementById("email").value;
+    // se houver algum endereço de email no input 
+   if (email = document.getElementById("email").value){
+        window.alert("Um link de endereço foi  enviado para o seu email \n "+ email);
     }
+    // se não for entroduzido nenhum endereço de email no input 
     else{
         window.alert("Insira o seu E-mail para que possamos enviar-lhe um link de revalidação ")
 
@@ -17,6 +20,6 @@ function forgotpassword() {
     
 }
 
-// && email.includes("@")
+
 
 
--- java.script/signup.js
+++ java.script/signup.js
@@ -1,8 +1,9 @@
 
 function sign_form_upValidate(){
+    // aceder ao valor dos elementos html com id="Fname" e "Lname"
     let Fname = document.getElementById('Fname').value;
     let Lname = document.getElementById('Lname').value;
-
+       // msg de alerta e concatenação do username
     window.alert("Bem vindo(a)! \n" + Fname + " " + Lname);
 
 }
